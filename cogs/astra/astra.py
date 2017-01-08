aaaf357a8a0675b936ea3cf332c836820f9ab8f3
GitAcrown <Acrown@outlook.fr>
2017-01-08 01:33:33 +0100

    MAJ Astra

--- cogs/astra/astra.py
+++ cogs/astra/astra.py
@@ -18,12 +18,220 @@ class Astra:
         self.case = dataIO.load_json("data/astra/case.json")
         self.sys = dataIO.load_json("data/astra/sys.json")
         self.logs = dataIO.load_json("data/astra/logs.json")
+        self.ddb = dataIO.load_json("data/astra/ddb.json")
+
+    @commands.command(pass_context=True)
+    async def signal(self, ctx, user : discord.Member, *raison):
+        """Permet de signaler (DDB) un utilisateur à la modération."""
+        author = ctx.message.author
+        raison = " ".join(raison)
+        if user.id not in self.ddb:
+            self.ddb[user.id] = []
+            for e in self.ddb[user.id]:
+                if author.id == e[0]:
+                    await self.bot.whisper("Vous avez déjà signalé cet utilisateur.")
+                    return
+            else:
+                self.ddb[user.id].append([author.id, raison])
+                fileIO("data/astra/ddb.json", "save", self.ddb)
+                await self.bot.whisper("Utilisateur signalé.")
+        else:
+            for e in self.ddb[user.id]:
+                if author.id == e[0]:
+                    await self.bot.whisper("Vous avez déjà signalé cet utilisateur.")
+                    return
+            else:
+                self.ddb[user.id].append([author.id, raison])
+                fileIO("data/astra/ddb.json", "save", self.ddb)
+                await self.bot.whisper("Utilisateur signalé.")
 
     @commands.group(pass_context=True)
     async def astra(self, ctx):
         """Gestion de ASTRA"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
+
+    @astra.command(pass_context=True)
+    async def suser(self, ctx, *snom):
+        """Recherche un utilisateur."""
+        server = ctx.message.server
+        nom = " ".join(snom).lower()
+        nb = 0
+        for member in server.members: #On cherche le nombre de résultats [RECHERCHE ALL]
+            if nom in member.name or nom in member.display_name:
+                nb += 1
+        if nb > 0:
+            await self.bot.say("**{} Résultat(s)**".format(nb))
+            await asyncio.sleep(0.25)
+            msg = "**Quel mode voulez-vous utiliser ?**\n"
+            msg += "*ALL* - Affiche l'ensemble des résultats\n"
+            msg += "*DEB* - Affiche les résultats commençant par votre recherche\n"
+            msg += "*FIN* - Affiche les résultats finissant par votre recherche\n"
+            msg += "-----------------------\n"
+            msg += "*ACTIF* - Affiche seulement les membres actifs\n"
+            msg += "*INACT* - Affiche seulement les membres inactifs\n"
+            msg += "*ROLE* - Affiche seulement les membres possédant le rôle visé\n"
+            msg += "*STR* - Recherche stricte, la case est prise en compte\n"
+            msg += "\n" + "**Assemblez les mots clefs ci-dessus pour lancer la recherche...**\n*Note : N'oubliez pas qu'il faut au moins 'ALL','DEB' ou 'FIN' pour démarrer la recherche !*"
+            await self.bot.say(msg)
+            verif = False
+            while verif != True:
+                rps = await self.bot.wait_for_message(author = ctx.message.author, channel = ctx.message.channel)
+                rps = rps.content.lower()
+                if rps != "":
+                    
+                    if "role" in rps:
+                        verif2 = False
+                        await self.bot.say("**Mentionne le(s) rôle(s) que tu veux rechercher:**")
+                        while verif2 != True:
+                            rmsg = await self.bot.wait_for_message(author = ctx.message.author, channel = ctx.message.channel)
+                            if rmsg.content != "":
+                                rlist = rmsg.role_mentions
+                                verif2 = True
+                            else:
+                                await self.bot.say("Invalide, réessaye !")
+                                continue
+                                
+                    if "str" in rps:
+                            nom = " ".join(snom)
+                    
+                    if "actif" and "inactif" in rps:
+                        await self.bot.say("Certains mots clefs ne sont pas compatibles entre eux ! ('ACTIF' et 'INACT' reviennent à 'ALL' !)\nRéessayez.")
+                        continue
+                    
+                    #RECHERCHE
+                    verif = True
+                    if "all" in rps: #MODE FULL
+                        if "actif" in rps: #FULL ACTIF
+                            res = "**Voici ce que j'ai trouvé pour [ALL, ACTIF]:**\n"
+                            for member in server.members:
+                                if nom in member.name or nom in member.display_name:
+                                    if member.status is discord.Status.online:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "inact" in rps: #FULL INACTIF
+                            res = "**Voici ce que j'ai trouvé pour [ALL, INACT]:**\n"
+                            for member in server.members:
+                                if nom in member.name or nom in member.display_name:
+                                    if member.status is discord.Status.offline:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "role" in rps: #FULL ROLE
+                            res = "**Voici ce que j'ai trouvé pour [ALL, ROLE] (Avec les rôles spécifiés):**\n"
+                            for member in server.members:
+                                if nom in member.name or nom in member.display_name:
+                                    if self.compare_role(member, rlist):
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        else: #FULL SIMPLE
+                            res = "**Voici ce que j'ai trouvé pour [ALL]:**\n"
+                            for member in server.members:
+                                if nom in member.name or nom in member.display_name:
+                                    res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        await self.bot.say(res)
+                    elif "deb" in rps: #MODE DEBUT
+                        r = len(nom)
+                        if "actif" in rps: #DEBUT ACTIF
+                            res = "**Voici ce que j'ai trouvé pour [DEB, ACTIF]:**\n"
+                            for member in server.members:
+                                if nom in member.name[:r] or nom in member.display_name[:r]:
+                                    if member.status is discord.Status.online:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "inact" in rps: #DEBUT INACT
+                            res = "**Voici ce que j'ai trouvé pour [DEB, INACT]:**\n"
+                            for member in server.members:
+                                if nom in member.name[:r] or nom in member.display_name[:r]:
+                                    if member.status is discord.Status.offline:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "role" in rps: #DEBUT ROLE
+                            res = "**Voici ce que j'ai trouvé pour [DEB, ROLE] (Avec les rôles spécifiés):**\n"
+                            for member in server.members:
+                                if nom in member.name[:r] or nom in member.display_name[:r]:
+                                    if self.compare_role(member, rlist):
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        else: #DEBUT SIMPLE
+                            res = "**Voici ce que j'ai trouvé pour [DEB]:**\n"
+                            for member in server.members:
+                                if nom in member.name[:r] or nom in member.display_name[:r]:
+                                    res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        await self.bot.say(res)
+                    elif "fin" in rps: #MODE FIN
+                        r = len(nom)
+                        if "actif" in rps: #FIN ACTIF
+                            res = "**Voici ce que j'ai trouvé pour [FIN, ACTIF]:**\n"
+                            for member in server.members:
+                                if nom in member.name[-r:] or nom in member.display_name[-r:]:
+                                    if member.status is discord.Status.online:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "inact" in rps: #FIN INACT
+                            res = "**Voici ce que j'ai trouvé pour [FIN, INACT]:**\n"
+                            for member in server.members:
+                                if nom in member.name[-r:] or nom in member.display_name[-r:]:
+                                    if member.status is discord.Status.offline:
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        elif "role" in rps: #FIN ROLE
+                            res = "**Voici ce que j'ai trouvé pour [FIN, ROLE] (Avec les rôles spécifiés):**\n"
+                            for member in server.members:
+                                if nom in member.name[-r:] or nom in member.display_name[-r:]:
+                                    if self.compare_role(member, rlist):
+                                        res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        else: #FIN SIMPLE
+                            res = "**Voici ce que j'ai trouvé pour [FIN]:**\n"
+                            for member in server.members:
+                                if nom in member.name[-r:] or nom in member.display_name[-r:]:
+                                    res += "*{}* | {}\n".format(member.display_name, member.mention)
+                        await self.bot.say(res)
+                    else:
+                        await self.bot.say("Votre recherche ne donne aucun résultat. Vérifiez vos mots-clefs.")
+                        continue
+                else:
+                    await self.bot.say("Mettez des mots clefs pour commencer une recherche.")
+                    continue
+
+
+
+    @astra.group(pass_context=True)
+    @checks.mod_or_permissions(kick_members=True)
+    async def sin(self, ctx):
+        """Collection de commandes Signal"""
+        if ctx.invoked_subcommand is None or \
+                isinstance(ctx.invoked_subcommand, commands.Group):
+            await send_cmd_help(ctx)
+            return
+
+    @sin.command(pass_context=True)
+    async def box(self, ctx, user : discord.Member = None):
+        """Permet de voir les utilisateurs signalés.
+
+        Si l'utilisateur est précisé, permet de voir ses signalements."""
+        server = ctx.message.server
+        if user is None:
+            msg = "**__Utilisateurs signalés :__**\n"
+            for u in self.ddb:
+                mem = server.get_member(u)
+                msg += "- *{}*\n".format(mem.display_name)
+            await self.bot.say(msg)
+        else:
+            if user.id in self.ddb:
+                msg = "**__Signalements pour {} :__**\n".format(user.name)
+                for p in self.ddb[user.id]:
+                    util = server.get_member(p[0])
+                    msg += "**{}** | *{}*\n".format(util.name, p[1])
+                await self.bot.say(msg)
+
+    @sin.command(pass_context=True)
+    async def done(self, ctx, user : discord.Member):
+        """Permet d'indiquer qu'un signalement a été pris en compte."""
+        if user.id in self.ddb:
+            del self.ddb[user.id]
+            await self.bot.say("Signalement retiré.")
+            fileIO("data/astra/ddb.json", "save", self.ddb)
+        else:
+            await self.bot.say("Cet utilisateur n'est pas présent.")
+
+    @sin.command(pass_context=True)
+    async def clean(self, ctx):
+        """Efface l'ensemble des signalements."""
+        self.ddb = {}
+        fileIO("data/astra/ddb.json", "save", self.ddb)
+        await self.bot.say("L'ensemble des signalements ont été supprimés.")
 
     @astra.command(pass_context=True)
     @checks.mod_or_permissions(administrator=True)
@@ -384,6 +592,10 @@ def check_files():
     if not os.path.isfile("data/astra/logs.json"):
         print("Création du fichier de stockage Astra...")
         fileIO("data/astra/logs.json", "save", {})
+
+    if not os.path.isfile("data/astra/ddb.json"):
+        print("Création du fichier de signalement Astra...")
+        fileIO("data/astra/ddb.json", "save", {})
 
 def setup(bot):
     check_folders()
