0b22cac79e81acc6ff65a9ad2af3070f0f83bb93
GitAcrown <Acrown@outlook.fr>
2017-08-26 00:38:46 +0200

    Add files via upload

--- cogs/just/just.py
+++ cogs/just/just.py
@@ -16,6 +16,20 @@ class Just:
         self.bot = bot
         self.sys = dataIO.load_json("data/just/sys.json")
         self.reg = dataIO.load_json("data/just/reg.json")
+        self.cycle_task = bot.loop.create_task(self.reduce())
+
+    async def reduce(self):
+        await self.bot.wait_until_ready()
+        try:
+            await asyncio.sleep(20)  # Temps de mise en route
+            while True:
+                for p in self.reg:
+                    if self.reg[p]["BANG"] >= 1:
+                        self.reg[p]["BANG"] -= 1
+                self.save()
+                await asyncio.sleep(20)  # Toutes les 24h
+        except asyncio.CancelledError:
+            pass
 
     def save(self):
         fileIO("data/just/sys.json", "save", self.sys)
@@ -43,7 +57,7 @@ class Just:
     @commands.command(aliases=["p", "jail"], pass_context=True)
     @checks.admin_or_permissions(manage_messages=True)
     async def prison(self, ctx, user: discord.Member, temps: str = "5m"):
-        """Mettre un membre en prison pendant un certain temps.
+        """Mettre un membre en prison pendant un certain temps (Par défaut 5m).
         
         user = Utilisateur à mettre en prison
         temps = Temps pendant lequel l'utilisateur est en prison (Minimum 60s)
@@ -121,8 +135,11 @@ class Just:
                     await self.bot.send_message(user, "**Tu as été mis(e) en prison pendant {}**\nSortie prévue à: `{}`\n"
                                                       "Un salon textuel écrit est disponible sur le serveur afin de contester cette punition ou afin d'obtenir plus d'informations.".format(temps, estim))
                     self.save()
-                    while time.time() < self.reg[user.id]["FIN_PEINE"]:
-                        await asyncio.sleep(2)
+                    try:
+                        while time.time() < self.reg[user.id]["FIN_PEINE"] or self.reg[user.id]["FIN_PEINE"] != None:
+                            await asyncio.sleep(1)
+                    except:
+                        pass
                     if user in server.members:
                         if role in [r.name for r in user.roles]:
                             self.reg[user.id]["DEB_PEINE"] = self.reg[user.id]["FIN_PEINE"] = None
@@ -143,6 +160,125 @@ class Just:
             else:
                 await self.bot.say("Le temps minimum est de 1m")
 
+    @commands.command(pass_context=True)
+    async def niv(self, ctx, user:discord.Member = None):
+        """Permet de voir le niveau Bang de quelqu'un ou de soit-même."""
+        if user is None:
+            user = ctx.message.author
+        if user.id in self.reg:
+            await self.bot.say("**{}** est au niveau **{}**.".format(user.name, self.reg[user.id]["BANG"]))
+        else:
+            await self.bot.say("L'utilisateur n'a pas de casier.")
+
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(administrator=True)
+    async def bangp(self, ctx):
+        """Permet de régler les punitions affligées par Bangbang.
+        
+        (Admin seulement)"""
+        author = ctx.message.author
+        if "BANG_PUN" not in self.sys:
+            self.sys["BANG_PUN"] = {"1": {"CLASS": "prison", "REGLAGE": "5m"},
+                                    "2": {"CLASS": "prison", "REGLAGE": "1h"},
+                                    "3": {"CLASS": "prison", "REGLAGE": "12h"},
+                                    "4": {"CLASS": "kick", "REGLAGE": None}}
+        while True:
+            msg = ""
+            for e in self.sys["BANG_PUN"]:
+                msg += "**{}** - *{}* {}\n".format(e, self.sys["BANG_PUN"][e]["CLASS"], "({})".format(self.sys["BANG_PUN"][e]["REGLAGE"] if self.sys["BANG_PUN"][e]["REGLAGE"] is not None else ""))
+            em = discord.Embed(title="Réglages Bangbang", description=msg, color=0xFE2E2E)
+            em.set_footer(text="Tapez le numéro correspondant au niveau de punition pour la modifier (Ignorez pour quitter)")
+            m = await self.bot.whisper(embed=em)
+            t = False
+            while t is False:
+                rep = await self.bot.wait_for_message(channel=m.channel, author=author, timeout=30)
+                if rep is None:
+                    await self.bot.whisper("Timeout atteint - Annulation :wave:")
+                    return
+                elif rep.content in self.sys["BANG_PUN"]:
+                    t = True
+                    await self.bot.whisper("**Punitions disponibles :**\n- Prison (De 1m à 1j)\n- Kick\n- Warning (Message de prévention)\n- Retrogradation (de rôle)\n\n"
+                                       "Choisissez votre punition en tapant son nom (Ex: 'prison' pour la Prison ou 'kick' pour Kick) ou tapez 'annuler' pour retourner en arrière.")
+                    t2 = False
+                    while t2 is False:
+                        rap = await self.bot.wait_for_message(channel=m.channel, author=author, timeout=30)
+                        if rap is None:
+                            await self.bot.whisper("Timeout atteint - Annulation :wave:")
+                            return
+                        elif rap.content.lower() == "prison":
+                            t2 = True
+                            await self.bot.whisper("Combien de temps ? Formats: s, m, h, j\n*Exemples: 90s, 8m, 10h, 1j...*")
+                            t3 = False
+                            while t3 is False:
+                                rop = await self.bot.wait_for_message(channel=m.channel, author=author, timeout=30)
+                                if rop is None:
+                                    await self.bot.whisper("Timeout atteint - Annulation :wave:")
+                                    return
+                                elif len(rop.content) >= 2:
+                                    if not rop.content.isdigit():
+                                        self.sys["BANG_PUN"][rep.content] = {"CLASS": "prison", "REGLAGE": rop.content.lower()}
+                                        self.save()
+                                        await self.bot.whisper("Punition réglée avec succès - Retour au menu...")
+                                        t3 = True
+                                    else:
+                                        await self.bot.whisper("Le format n'est pas bon. Lisez bien")
+                                else:
+                                    await self.bot.whisper("Le format n'est pas bon. Lisez bien")
+                        elif rap.content.lower() == "kick":
+                            t2 = True
+                            self.sys["BANG_PUN"][rep.content] = {"CLASS": "kick", "REGLAGE": None}
+                            self.save()
+                            await self.bot.whisper("Punition réglée avec succès - Retour au menu...")
+                        elif rap.content.lower() == "warning":
+                            t2 = True
+                            await self.bot.whisper(
+                                "Quel message doit être envoyé ? (>5 caractères)")
+                            t3 = False
+                            while t3 is False:
+                                rop = await self.bot.wait_for_message(channel=m.channel, author=author, timeout=30)
+                                if rop is None:
+                                    await self.bot.whisper("Timeout atteint - Annulation :wave:")
+                                    return
+                                elif len(rop.content) >= 5:
+                                    self.sys["BANG_PUN"][rep.content] = {"CLASS": "warning",
+                                                                         "REGLAGE": rop.content}
+                                    self.save()
+                                    await self.bot.whisper("Punition réglée avec succès - Retour au menu...")
+                                    t3 = True
+                                else:
+                                    await self.bot.whisper("Le format n'est pas bon. Lisez bien")
+                        elif rap.content.lower() == "retrogradation":
+                            t2 = True
+                            await self.bot.whisper(
+                                "Il y a t'il des rôles que je ne dois pas retirer ? (Dans ce cas, je retire celui directement en dessous) Sinon tapez 'non'.\n**Format:** role1;role2;role3...\n*Donnez les noms de rôles EXACTS (Majuscule comprise), sinon ça ne fonctionnera pas.*")
+                            t3 = False
+                            while t3 is False:
+                                rop = await self.bot.wait_for_message(channel=m.channel, author=author, timeout=30)
+                                if rop is None:
+                                    await self.bot.whisper("Timeout atteint - Annulation :wave:")
+                                    return
+                                elif rop.content.lower() == "non":
+                                    self.sys["BANG_PUN"][rep.content] = {"CLASS": "retro",
+                                                                         "REGLAGE": []}
+                                    self.save()
+                                    await self.bot.whisper("Punition réglée avec succès - Retour au menu...")
+                                    t3 = True
+                                elif len(rop.content.split(";")) > 1:
+                                    liste = rop.content.split(";")
+                                    self.sys["BANG_PUN"][rep.content] = {"CLASS": "retro",
+                                                                         "REGLAGE": liste}
+                                    self.save()
+                                    await self.bot.whisper("Punition réglée avec succès - Retour au menu...")
+                                    t3 = True
+                                else:
+                                    await self.bot.whisper("Invalide. Lisez bien les directives ci-dessus.")
+                        elif rap.content.lower() == "annuler":
+                            t2 = True
+                        else:
+                            await self.bot.whisper("Invalide, réessayez.")
+                else:
+                    await self.bot.say("Invalide. Tapez le numéro correspondant au niveau que vous voulez modifier.")
+
     async def renew(self, user):
         server = user.server
         chanp = self.bot.get_channel("204585334925819904")
@@ -157,8 +293,8 @@ class Just:
                         estim = time.strftime("%H:%M", time.localtime(self.reg[user.id]["FIN_PEINE"]))
                         await self.bot.send_message(chanp, "**{} retourne automatiquement en prison pour finir sa peine**".format(user.name))
                         await self.bot.send_message("**Rappel: Vous aviez été mis en prison et votre peine n'est pas terminée.**\nSortie prévue à: `{}`".format(estim))
-                        while time.time() < self.reg[user.id]["FIN_PEINE"]:
-                            await asyncio.sleep(2)
+                        while time.time() < self.reg[user.id]["FIN_PEINE"] or self.reg[user.id]["FIN_PEINE"] != None:
+                            await asyncio.sleep(1)
                         if user in server.members:
                             if role in [r.name for r in user.roles]:
                                 self.reg[user.id]["DEB_PEINE"] = self.reg[user.id]["FIN_PEINE"] = None
@@ -169,14 +305,96 @@ class Just:
                             else:
                                 return
                         else:
-                            await self.bot.send_message(chanp, "{} ne peut être libéré de prison car il est n'est plus sur ce serveur.".format(save))
+                            await self.bot.send_message(chanp, "{} ne peut être libéré de prison car il n'est plus sur ce serveur.".format(save))
 
     async def bang(self, reaction, author):
         user = reaction.message.author
+        role = self.sys["ROLE_PRISON"]
+        save = user.name
+        server = user.server
+        chanp = self.bot.get_channel("329071582129422337")
+        apply = discord.utils.get(server.roles, name=role)
         if reaction.emoji == "‼":
             if author.server_permissions.manage_messages is True:
-                await self.bot.send_message(author, "**Fonction en pleine refonte**\nVisitez https://github.com/GitAcrown/Addon/issues/3 pour suivre l'avancement du développement.")
-                return
+                if "BANG_PUN" in self.sys:
+                    if user not in self.reg:
+                        self.reg[user.id] = {"FIN_PEINE": None,
+                                             "DEB_PEINE": None,
+                                             "BANG": 0,
+                                             "ROLES": [r.name for r in user.roles],
+                                             "D_PSEUDO": user.display_name,
+                                             "TRACKER": []}
+                        self.save()
+                    #Début
+                    if self.reg[user.id]["BANG"] < 4:
+                        self.reg[user.id]["BANG"] += 1
+                        self.save()
+                    else:
+                        await self.bot.send_message(author, "Le membre est déjà au niveau maximal.\n**La punition de niveau 4 est répétée.**")
+                    pun = self.sys["BANG_PUN"][str(self.reg[user.id]["BANG"])]["CLASS"]
+                    if pun == "prison":
+                        if role not in [r.name for r in user.roles]:
+                            plus = self.sys["BANG_PUN"][str(self.reg[user.id]["BANG"])]["REGLAGE"]
+                            form = plus[-1:]
+                            val = int(plus.replace(form, ""))
+                            sec = self.convert_sec(form, val)
+                            b_peine = time.time()
+                            self.reg[user.id]["DEB_PEINE"] = b_peine
+                            self.reg[user.id]["FIN_PEINE"] = b_peine + sec
+                            self.save()
+                            await self.bot.add_roles(user, apply)
+                            estim = time.strftime("%H:%M", time.localtime(self.reg[user.id]["FIN_PEINE"]))
+                            await self.bot.send_message(chanp,
+                                                        "**{} est envoyé en prison pour {} par {}**".format(
+                                                            user.name, plus, author.name))
+                            await self.bot.send_message(user,
+                                "**Vous avez été mis en prison dans le cadre d'une punition.**\nSortie prévue à: `{}`\nUn salon textuel est disponible sur le serveur afin de contester cette décision.".format(
+                                    estim))
+                            while time.time() < self.reg[user.id]["FIN_PEINE"]:
+                                await asyncio.sleep(1)
+                            if user in server.members:
+                                if role in [r.name for r in user.roles]:
+                                    self.reg[user.id]["DEB_PEINE"] = self.reg[user.id]["FIN_PEINE"] = None
+                                    self.new_event("out", user.id, "auto")
+                                    await self.bot.remove_roles(user, apply)
+                                    await self.bot.send_message(chanp, "{} **est libre**".format(user.mention))
+                                    self.save()
+                                else:
+                                    return
+                            else:
+                                await self.bot.send_message(chanp,
+                                                            "{} ne peut être libéré de prison car il n'est plus sur ce serveur.".format(
+                                                                save))
+                        else:
+                            await self.bot.send_message(chanp, "Punition abandonnée - Le membre est déjà en train du purger une peine de prison.")
+                            return
+                    elif pun == "kick":
+                        await self.bot.kick(user)
+                        await self.bot.send_message("**{}** a été kick par **{}**".format(save, author.name))
+                        return
+                    elif pun == "retro":
+                        liste = self.sys["BANG_PUN"][str(self.reg[user.id]["BANG"])]["REGLAGE"]
+                        roles = [r.name for r in user.roles]
+                        roles.remove("@everyone")
+                        if roles != []:
+                            fait = False
+                            num = user.top_role.position
+                            while fait is False:
+                                if roles[num] in liste:
+                                    ret = discord.utils.get(server.roles, name=roles[num])
+                                    await self.bot.remove_roles(user, ret)
+                                    fait = True
+                                else:
+                                    num -= 1
+                            await self.bot.send_message(chanp, "**{}** à eu son rôle *{}* retiré.".format(user.name, roles[num]))
+                            return
+                        else:
+                            await self.bot.send_message(chanp, "L'utilisateur n'a pas de rôles, il ne peut être rétrogradé.")
+                    elif pun == "warning":
+                        await self.bot.send_message(chanp, "{} **{}**".format(user.mention, self.sys["BANG_PUN"][str(self.reg[user.id]["BANG"])]["REGLAGE"]))
+                    else:
+                        pass
+                    #TODO Mise en surveillance (Garde)
 
 def check_folders():
     folders = ("data", "data/just/")
